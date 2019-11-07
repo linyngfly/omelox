b7671382dd632d8fa62a007f441d5073159a8bc9
yng <linyngfly@163.com>
2019-11-07 17:18:06 +0800

    abc

--- tools/omelox-robot/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/master/server';
-export * from './lib/agent/agent';
-export * from './lib/agent/actor';
-module.exports.HTTP_SERVER = require('./lib/console/http').HTTP_SERVER;
-export * from './lib/robot';
