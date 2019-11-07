1e0d86a2279cdc694b48aa382e39b29f2854e224
yng <yng@gmail.com>
2019-11-07 17:40:26 +0800

    删除示例项目

--- examples/websocket-chat-ts-run/game-server/config/serverProtos.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'onChat': {
-        'required string msg': 1,
-        'required string from': 2,
-        'required string target': 3
-    }
-};
--- examples/websocket-chat/game-server/app/util/dispatcher.ts
+++
@@ -1,7 +0,0 @@
-import * as crc from 'crc';
-import { ServerInfo } from 'omelox';
-
-export function dispatch(uid: string , connectors: ServerInfo[]) {
-    let index = Math.abs(crc.crc32(uid)) % connectors.length;
-    return connectors[index];
-}
