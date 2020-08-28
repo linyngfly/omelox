4dc41e0fd8ded73b3f60bbd878aa4bc69a3b8525
yng <yng@gmail.com>
2020-08-28 22:33:56 +0800

    sync

--- packages/omelox/template/game-server/app/plugins/todo.ts
+++ packages/omelox/template/game-server/app/plugins/todo.ts
@@ -1,1 +1,10 @@
 // 业务插件
+// 微服务架构  https://github.com/tars-node        https://github.com/TarsCloud/Tars
+
+// https://github.com/TarsCloud/Tars/blob/master/README.zh.md
+
+// https://nestcloud.org/#/zh-cn/README
+
+// https://github.com/moleculerjs/moleculer
+
+// https://segmentfault.com/a/1190000008501410#articleHeader0
