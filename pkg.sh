9594293eb8558494659204a4013a05361a1d3eb4
yng <yng@gmail.com>
2020-09-01 12:22:13 +0800

    sync

---
+++ pkg.sh
@@ -0,0 +1,9 @@
+npm publish packages/omelox 
+npm publish packages/omelox-admin 
+npm publish packages/omelox-loader 
+npm publish packages/omelox-logger 
+npm publish packages/omelox-monitor 
+npm publish packages/omelox-protobuf 
+npm publish packages/omelox-protocol 
+npm publish packages/omelox-rpc 
+npm publish packages/omelox-scheduler
