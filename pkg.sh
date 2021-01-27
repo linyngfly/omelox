256ed8a43bb1002d629f72c6b3aaf056776d6769
yng <yng@gmail.com>
2021-01-27 11:41:41 +0800

    add sync

--- pkg.sh
+++ pkg.sh
@@ -1,2 +1,19 @@
-npm publish packages/omelox & npm publish packages/omelox-admin & npm publish packages/omelox-loader & npm publish packages/omelox-logger 
-npm publish packages/omelox-monitor & npm publish packages/omelox-protobuf & npm publish packages/omelox-protocol & npm publish packages/omelox-rpc & npm publish packages/omelox-scheduler
+npm publish packages/omelox 
+npm publish packages/omelox-admin
+npm publish packages/omelox-loader
+npm publish packages/omelox-logger 
+npm publish packages/omelox-monitor
+npm publish packages/omelox-protobuf
+npm publish packages/omelox-protocol 
+npm publish packages/omelox-rpc
+npm publish packages/omelox-scheduler
+npm publish plugins/omelox-globalchannel-plugin
+npm publish plugins/omelox-globalstatus-plugin
+npm publish plugins/omelox-http-plugin
+npm publish plugins/omelox-robot-plugin
+npm publish tools/omelox-cli
+npm publish tools/omelox-excel
+npm publish tools/omelox-gen-route
+npm publish tools/omelox-json-schema
+npm publish tools/omelox-protoc
+npm publish tools/omelox-robot
