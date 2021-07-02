6bd9ddbd7910219dfd2575087a1e3e305cec97a9
linyng <linyngfly@163.com>
2021-07-02 18:25:23 +0800

    同步社区修改20210702

--- packages/omelox-logger/lib/logger.ts
+++ packages/omelox-logger/lib/logger.ts
@@ -20,6 +20,12 @@ function setOmeloxLogLevel(newLevel: 0 | 1 | 2 | 3 | 4 | 5) {
     logLevel = newLevel;
 }
 
+let log4jspause = false;
+
+process.on('log4js:pause' as any, (val: any) => {
+    log4jspause = val;
+});
+
 function getLogger(...args: string[]) {
     let categoryName = args[0];
     let prefix = '';
@@ -43,7 +49,7 @@ function getLogger(...args: string[]) {
     ['log', 'debug', 'info', 'warn', 'error', 'trace', 'fatal'].forEach((item, idx) => {
         pLogger[item] = function () {
             // 从根源过滤日志级别
-            if (idx < logLevel) {
+            if (idx < logLevel || log4jspause) {
                 return;
             }
             let p = '';
