ef8ea872115634445e17bb74c614dd58f4de1d36
gofly <linyngfly@163.com>
2021-11-19 06:40:40 +0800

    add fix

--- tools/omelox-json-schema/lib/utils.ts
+++ tools/omelox-json-schema/lib/utils.ts
@@ -2,8 +2,24 @@
 import * as fs from 'fs';
 import { parseToJsonSchem } from './main';
 
+const protoInts = ['uInt32', 'sInt32', 'int32'];
+const protoFloats = ['double', 'float'];
+
+
+function protoType2JsonSchemaType(schemaDefine: string) {
+    protoInts.forEach((item) => {
+        schemaDefine = schemaDefine.split(`"type": \"${item}\"`).join(`"type": "integer"`)
+    })
+
+    protoFloats.forEach((item) => {
+        schemaDefine = schemaDefine.split(`"type": \"${item}\"`).join(`"type": "number"`)
+    })
+
+    return schemaDefine;
+}
+
 export function parseAndWrite(sourcePath: string, clientPath: string, serverPath: string) {
     const result = parseToJsonSchem(sourcePath);
-    fs.writeFileSync(clientPath, JSON.stringify(result.client, null, 4));
-    fs.writeFileSync(serverPath, JSON.stringify(result.server, null, 4));
+    fs.writeFileSync(clientPath, protoType2JsonSchemaType(JSON.stringify(result.client, null, 4)));
+    fs.writeFileSync(serverPath, protoType2JsonSchemaType(JSON.stringify(result.server, null, 4)));
 }
