0afbc10ebabd57266df2bee4b98a953a4806ae1b
gofly <linyngfly@163.com>
2022-01-03 23:23:13 +0800

    update version

--- pkg.sh
+++ pkg.sh
@@ -1,22 +1,22 @@
-npm publish packages/omelox 
-npm publish packages/omelox-admin
-npm publish packages/omelox-loader
-npm publish packages/omelox-logger 
-npm publish packages/omelox-monitor
-npm publish packages/omelox-protobuf
-npm publish packages/omelox-protocol 
-npm publish packages/omelox-rpc
-npm publish packages/omelox-scheduler
-npm publish plugins/omelox-globalchannel-plugin
-npm publish plugins/omelox-globalstatus-plugin
-npm publish plugins/omelox-http-plugin
-npm publish plugins/omelox-robot-plugin
-npm publish tools/omelox-cli
-npm publish tools/omelox-excel
-npm publish tools/omelox-gen-route
-npm publish tools/omelox-json-schema
-npm publish tools/omelox-protoc
-npm publish tools/omelox-robot
+# npm publish packages/omelox 
+# npm publish packages/omelox-admin
+# npm publish packages/omelox-loader
+# npm publish packages/omelox-logger 
+# npm publish packages/omelox-monitor
+# npm publish packages/omelox-protobuf
+# npm publish packages/omelox-protocol 
+# npm publish packages/omelox-rpc
+# npm publish packages/omelox-scheduler
+# npm publish plugins/omelox-globalchannel-plugin
+# npm publish plugins/omelox-globalstatus-plugin
+# npm publish plugins/omelox-http-plugin
+# npm publish plugins/omelox-robot-plugin
+# npm publish tools/omelox-cli
+# npm publish tools/omelox-excel
+# npm publish tools/omelox-gen-route
+# npm publish tools/omelox-json-schema
+# npm publish tools/omelox-protoc
+# npm publish tools/omelox-robot
 
 # 一键发布
-# lerna publish from-package
+lerna publish from-package
